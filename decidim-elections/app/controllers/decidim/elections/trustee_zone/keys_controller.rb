9e82c7abcd2a1bdd2f2e03da8f6668b669b9a5d0
David Morcillo <david@codegram.com>
2020-12-10 17:03:16 +0100

    feat: add cast action and views

---
+++ decidim-elections/app/controllers/decidim/elections/trustee_zone/keys_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Elections
+    module TrusteeZone
+      # Handles the KeyCeremony for trustee users
+      class KeysController < ::Decidim::ApplicationController
+        include Decidim::UserProfile
+
+        helper_method :election, :trustee
+
+        private
+
+        def election
+          @election ||= Decidim::Elections::Election.find(params[:election_id])
+        end
+
+        def trustee
+          @trustee ||= Decidim::Elections::Trustee.for(current_user)
+        end
+      end
+    end
+  end
+end
